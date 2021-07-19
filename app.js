fb302d6c66c5a207c8965ac680c7e8f1a02f2818
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-07-19 15:50:18 +0000

    added that obj into template

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const app = Vue.createApp({  //Vue => we have access to this obj, because of our CDN we have in html(script tag for Vue)
-  template: '<h1>Hello Wolrd</h1>',
+  template: '<h1>Hello {{firstName}}</h1>',
   // data is func that returns an obj
   data() {
     return {
-      
+      firstName: "John"
     }
   }
 });
